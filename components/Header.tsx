40e7ee3ab4e1bf65602c30387c991bdb7b79582a
thahertech <thaher.alamir@hotmail.com>
2025-01-28 22:05:10 +0200

    header added

---
+++ components/Header.tsx
@@ -0,0 +1,33 @@
+// components/Header.tsx
+import Link from "next/link";
+
+const Header: React.FC = () => {
+  return (
+    <header className="bg-white-200 text-white p-4 shadow-md">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-xl font-bold">
+          <Link href="/">Kilpailutus</Link>
+        </h1>
+        <nav>
+          <ul className="flex space-x-4">
+            <li>
+              <Link href="/about" className="hover:underline ml-8">
+                Yrityksille
+              </Link>
+              <Link href="/about" className="hover:underline ml-8">
+                Yrityksille
+              </Link>
+            </li>
+            <li>
+              <Link href="/contact" className="hover:underline ml-4">
+                Miten kilpailutus toimii?
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
