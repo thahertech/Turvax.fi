3106002c37f85fdd22149afe5be5fea82adb7612
thahertech <thaher.alamir@hotmail.com>
2025-02-01 15:31:17 +0200

    g analytics

--- app/page.tsx
+++ app/page.tsx
@@ -67,7 +67,9 @@ export default function Home() {
 
   return (
     <>
-  <Script
+  <ConsentScript />
+
+      <Script
         strategy="afterInteractive"
         src="https://www.googletagmanager.com/gtag/js?id=G-8LRWN45FQ4"
         async
@@ -83,6 +85,7 @@ export default function Home() {
           gtag('config', 'G-8LRWN45FQ4');
         `}
       </Script>
+
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] overflow-none">
       <Header />
   
