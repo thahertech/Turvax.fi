4a2e0811f1ef3ec0a5ab320298515a86729775e1
thahertech <thaher.alamir@hotmail.com>
2025-03-17 20:11:39 +0200

    updates to selection system

--- components/serviceComponent.tsx
+++ components/serviceComponent.tsx
@@ -1,27 +1,36 @@
 import { useState } from "react";
 import { FiInfo } from "react-icons/fi";
 
-export default function ServiceSelection() {
+interface ServiceComponentProps {
+  onSelect: () => void;  // Add the onSelect prop to the component
+}
+
+export default function ServiceSelection({ onSelect }: ServiceComponentProps) {
   const services = ["Murto", "Paloturva", "Vesivahinko"];
   const [selectedServices, setSelectedServices] = useState<string[]>([]);
 
   const toggleService = (service: string) => {
-    setSelectedServices((prev: string[]) =>
-      prev.includes(service)
+    setSelectedServices((prev: string[]) => {
+      const newSelectedServices = prev.includes(service)
         ? prev.filter((s) => s !== service)
-        : [...prev, service]
-    );
+        : [...prev, service];
+
+      // Call onSelect after updating the state
+      onSelect();
+
+      return newSelectedServices;
+    });
   };
 
   return (
     <div>
       <label className="text-xl font-semibold flex items-center">
         Mistä palvelusta olet kiinnostunut?
-        <span className="ml-2 cursor-pointer">
+        {/* <span className="ml-2 cursor-pointer">
           <FiInfo title="Valintojen perusteella voit valita ominaisuudet kilpailutukselle" />
-        </span>
+        </span> */}
       </label>
-      <div className="mt-2 flex gap-2 flex-wrap">
+      <div className="mt-2 flex gap-2 flex-wrap justify-center m-auto">
         {services.map((service) => (
           <button
             key={service}
